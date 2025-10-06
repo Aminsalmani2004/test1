94daf14462ea83e3cc85b5dc51dc56cf03f6a853
Aminsalmani2004 <aminsalmanicoin2004@gmail.com>
2025-10-06 20:13:29 +0330

    delete items

--- js/Event.js
+++
@@ -1,7 +0,0 @@
-const ELEMENT = document.getElementById("mybtn");
-
-ELEMENT.addEventListener('click',displaydate);
-
-function displaydate(){
-    document.getElementById("test").innerHTML=Date();
-}
